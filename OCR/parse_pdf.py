4b6eba554241cf6577008744155cf5e559942944
arnavsam <arnavsam@gmail.com>
2023-12-01 12:08:42 -0500

    clean dir

--- OCR/parse_pdf.py
+++
@@ -1,5 +0,0 @@
-import scipdf
-article_dict = scipdf.parse_pdf_to_dict('article.pdf') # return dictionary
-print(article_dict)
-
-scipdf.parse_figures('sample.pdf', output_folder='figures') # folder should contain only PDF files
